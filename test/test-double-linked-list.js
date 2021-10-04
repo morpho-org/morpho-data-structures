0d84535482b833531ce83e09effd2e81961b0dd9
MerlinEgalite <egalite.merlin@gmail.com>
2021-10-04 21:46:48 +0200

    test(wip): add double linked list tests

---
+++ test/test-double-linked-list.js
@@ -0,0 +1,58 @@
+const { utils } = require('ethers');
+const { ethers } = require('hardhat');
+
+describe('Test DoubleLinkedList Library', () => {
+  let testDoubleLinkedList;
+  let addresses = [];
+  let addressesLength;
+
+  for (let i = 0; i < 500; i++) {
+    addresses.push(utils.solidityKeccak256(['uint256'], [i]).slice(0, 42));
+  }
+  addressesLength = addresses.length;
+
+  beforeEach(async () => {
+    const TestDoubleLinkedList = await ethers.getContractFactory('TestDoubleLinkedList');
+    testDoubleLinkedList = await TestDoubleLinkedList.deploy();
+    await testDoubleLinkedList.deployed();
+  });
+
+  describe('Test Gas Consumption', () => {
+    it('Test insert many values', async () => {
+      let value = utils.parseUnits('10');
+      for (let i = 0; i < addressesLength; i++) {
+        const address = addresses[i];
+        await testDoubleLinkedList.insertSorted(address, value);
+        value = value.sub(1);
+      }
+    });
+
+    it('Test remove many values (each time the first value)', async () => {
+      let value = utils.parseUnits('10');
+      for (let i = 0; i < addressesLength; i++) {
+        const address = addresses[i];
+        await testDoubleLinkedList.insertSorted(address, value);
+        value = value.sub(1);
+      }
+
+      for (let i = 0; i < addressesLength; i++) {
+        const address = addresses[i];
+        await testDoubleLinkedList.remove(address);
+      }
+    });
+
+    it('Test remove many values (each time the latest value)', async () => {
+      let value = utils.parseUnits('10');
+      for (let i = 0; i < addressesLength; i++) {
+        const address = addresses[i];
+        await testDoubleLinkedList.insertSorted(address, value);
+        value = value.sub(1);
+      }
+
+      for (let i = 0; i < addressesLength; i++) {
+        const address = addresses[addressesLength - i - 1];
+        await testDoubleLinkedList.remove(address);
+      }
+    });
+  });
+});
