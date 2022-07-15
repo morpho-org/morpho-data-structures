c2561e203dd5d5b92aab05039d9ca788bafefb02
Quentin Garchery <garchery.quentin@gmail.com>
2022-07-15 14:49:26 +0200

    Use indexes instead of ranks

--- test-foundry/TestStessHeapOrdering.t.sol
+++ test-foundry/TestStessHeapOrdering.t.sol
@@ -14,8 +14,8 @@ contract HeapStorage {
     function setUp() public {
         for (uint256 i = 0; i < TESTED_SIZE; i++) {
             address id = address(uint160(i + 1));
+            heap.indexes[id] = heap.accounts.length;
             heap.accounts.push(HeapOrdering.Account(id, uint96(TESTED_SIZE - i)));
-            heap.ranks[id] = heap.accounts.length;
             heap.size = MAX_SORTED_USERS;
         }
     }
