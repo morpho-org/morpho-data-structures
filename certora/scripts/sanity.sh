942c2bbef0b9890f66ea03bde437fa383349dfa1
Quentin Garchery <garchery.quentin@gmail.com>
2022-08-01 23:18:21 +0200

    fix: add DLL contract, prevent errors removing munged by accident

--- certora/scripts/sanity.sh
+++ certora/scripts/sanity.sh
@@ -3,8 +3,8 @@
 make -C certora munged
 
 certoraRun \
-    certora/munged/DoubleLinkedList.sol \
-    --verify DoubleLinkedList:certora/specs/sanity.spec \
+    certora/munged/DLL.sol \
+    --verify DLL:certora/specs/sanity.spec \
     --loop_iter 3 \
     --optimistic_loop \
     --send_only \
