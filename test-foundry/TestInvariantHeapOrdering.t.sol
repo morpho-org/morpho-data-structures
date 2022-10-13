6d1cbdc3bbc5960104b58b4edb3c4df8e148e305
MathisGD <mathis.gontier_delaunay@telecom-sudparis.eu>
2022-10-13 23:26:12 +0200

    test: improve heap invariant testing

--- test-foundry/TestInvariantHeapOrdering.t.sol
+++ test-foundry/TestInvariantHeapOrdering.t.sol
@@ -14,8 +14,10 @@ contract Heap is ConcreteHeapOrdering {
 
     /// Functions to fuzz ///
 
-    function update(address _id, uint96 _newValue) public {
-        heap.update(_id, heap.getValueOf(_id), _newValue, MAX_SORTED_USERS);
+    function updateCorrect(address _id, uint96 _newValue) public {
+        uint256 oldValue = heap.getValueOf(_id);
+        if (oldValue != 0 || _newValue != 0)
+            heap.update(_id, heap.getValueOf(_id), _newValue, MAX_SORTED_USERS);
     }
 }
 
@@ -35,7 +37,7 @@ contract TestHeapInvariant is DSTest {
     function targetSelectors() public view returns (FuzzSelector[] memory) {
         FuzzSelector[] memory targets = new FuzzSelector[](1);
         bytes4[] memory selectors = new bytes4[](1);
-        selectors[0] = Heap.update.selector;
+        selectors[0] = Heap.updateCorrect.selector;
         targets[0] = FuzzSelector(address(heap), selectors);
         return targets;
     }
@@ -47,12 +49,8 @@ contract TestHeapInvariant is DSTest {
         uint256 length = heap.length();
 
         for (uint256 i; i < length; ++i) {
-            assertTrue(
-                (i * 2 + 1 >= length || i * 2 + 1 >= heap.size() || heap.accountsValue(i) >= heap.accountsValue(i * 2 + 1))
-            ); // prettier-ignore
-            assertTrue(
-                (i * 2 + 2 >= length || i * 2 + 2 >= heap.size() || heap.accountsValue(i) >= heap.accountsValue(i * 2 + 2))
-            ); // prettier-ignore
+            assertTrue((i * 2 + 1 >= length || i * 2 + 1 >= heap.size() || heap.accountsValue(i) >= heap.accountsValue(i * 2 + 1))); // prettier-ignore
+            assertTrue((i * 2 + 2 >= length || i * 2 + 2 >= heap.size() || heap.accountsValue(i) >= heap.accountsValue(i * 2 + 2))); // prettier-ignore
         }
     }
 
