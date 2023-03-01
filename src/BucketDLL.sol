38415d36e815f8ff9dcf8c5af79ac68c9d5760b6
MathisGD <mathis.gontier_delaunay@telecom-sudparis.eu>
2023-03-01 11:24:41 +0100

    style: harmonize BucketDLL

--- src/BucketDLL.sol
+++ src/BucketDLL.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.0;
 
 library BucketDLL {
-    /// STRUCTS ///
+    /* STRUCTS */
 
     struct Account {
         address prev;
@@ -13,78 +13,78 @@ library BucketDLL {
         mapping(address => Account) accounts;
     }
 
-    /// INTERNAL ///
+    /* INTERNAL */
 
-    /// @notice Returns the address at the head of the `_list`.
-    /// @param _list The list from which to get the head.
+    /// @notice Returns the address at the head of the `list`.
+    /// @param list The list from which to get the head.
     /// @return The address of the head.
-    function getHead(List storage _list) internal view returns (address) {
-        return _list.accounts[address(0)].next;
+    function getHead(List storage list) internal view returns (address) {
+        return list.accounts[address(0)].next;
     }
 
-    /// @notice Returns the address at the tail of the `_list`.
-    /// @param _list The list from which to get the tail.
+    /// @notice Returns the address at the tail of the `list`.
+    /// @param list The list from which to get the tail.
     /// @return The address of the tail.
-    function getTail(List storage _list) internal view returns (address) {
-        return _list.accounts[address(0)].prev;
+    function getTail(List storage list) internal view returns (address) {
+        return list.accounts[address(0)].prev;
     }
 
-    /// @notice Returns the next id address from the current `_id`.
-    /// @param _list The list to search in.
-    /// @param _id The address of the current account.
+    /// @notice Returns the next id address from the current `id`.
+    /// @param list The list to search in.
+    /// @param id The address of the current account.
     /// @return The address of the next account.
-    function getNext(List storage _list, address _id) internal view returns (address) {
-        return _list.accounts[_id].next;
+    function getNext(List storage list, address id) internal view returns (address) {
+        return list.accounts[id].next;
     }
 
-    /// @notice Returns the previous id address from the current `_id`.
-    /// @param _list The list to search in.
-    /// @param _id The address of the current account.
+    /// @notice Returns the previous id address from the current `id`.
+    /// @param list The list to search in.
+    /// @param id The address of the current account.
     /// @return The address of the previous account.
-    function getPrev(List storage _list, address _id) internal view returns (address) {
-        return _list.accounts[_id].prev;
+    function getPrev(List storage list, address id) internal view returns (address) {
+        return list.accounts[id].prev;
     }
 
-    /// @notice Removes an account of the `_list`.
-    /// @dev This function should not be called with `_id` equal to address 0.
-    /// @param _list The list to search in.
-    /// @param _id The address of the account.
+    /// @notice Removes an account of the `list`.
+    /// @dev This function should not be called with `id` equal to address 0.
+    /// @param list The list to search in.
+    /// @param id The address of the account.
     /// @return Whether the bucket is empty after removal.
-    function remove(List storage _list, address _id) internal returns (bool) {
-        Account memory account = _list.accounts[_id];
+    function remove(List storage list, address id) internal returns (bool) {
+        Account memory account = list.accounts[id];
         address prev = account.prev;
         address next = account.next;
 
-        _list.accounts[prev].next = next;
-        _list.accounts[next].prev = prev;
+        list.accounts[prev].next = next;
+        list.accounts[next].prev = prev;
 
-        delete _list.accounts[_id];
+        delete list.accounts[id];
 
         return (prev == address(0) && next == address(0));
     }
 
-    /// @notice Inserts an account in the `_list`.
-    /// @dev This function should not be called with `_id` equal to address 0.
-    /// @param _list The list to search in.
-    /// @param _id The address of the account.
-    /// @param _head Tells whether to insert at the head or at the tail of the list.
+    /// @notice Inserts an account in the `list`.
+    /// @dev This function should not be called with `id` equal to address 0.
+    /// @param list The list to search in.
+    /// @param id The address of the account.
+    /// @param head Tells whether to insert at the head or at the tail of the list.
     /// @return Whether the bucket was empty before insertion.
     function insert(
-        List storage _list,
-        address _id,
-        bool _head
+        List storage list,
+        address id,
+        bool head
     ) internal returns (bool) {
-        if (_head) {
-            address head = _list.accounts[address(0)].next;
-            _list.accounts[address(0)].next = _id;
-            _list.accounts[head].prev = _id;
-            _list.accounts[_id].next = head;
+        if (head) {
+            address head = list.accounts[address(0)].next;
+            list.accounts[address(0)].next = id;
+            list.accounts[head].prev = id;
+            list.accounts[id].next = head;
             return head == address(0);
         } else {
-            address tail = _list.accounts[address(0)].prev;
-            _list.accounts[address(0)].prev = _id;
-            _list.accounts[tail].next = _id;
-            _list.accounts[_id].prev = tail;
+            address tail = list.accounts[address(0)].prev;
+            list.accounts[address(0)].prev = id;
+            list.accounts[tail].next = id;
+            list.accounts[id].prev = tail;
             return tail == address(0);
         }
     }
