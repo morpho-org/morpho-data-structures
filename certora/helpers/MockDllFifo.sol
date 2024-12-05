52a62ed2d01c0fc3a9c8c2b3e03213ee3a256a44
Quentin Garchery <garchery.quentin@gmail.com>
2024-12-05 14:14:25 +0100

    chore: named imports

--- certora/helpers/MockDllFifo.sol
+++ certora/helpers/MockDllFifo.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 pragma solidity ^0.8.0;
 
-import "../munged-fifo/DoubleLinkedList.sol";
+import {DoubleLinkedList} from "../munged-fifo/DoubleLinkedList.sol";
 
 contract MockDllFifo {
     using DoubleLinkedList for DoubleLinkedList.List;
--- certora/helpers/MockDllSimple.sol
+++ certora/helpers/MockDllSimple.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 pragma solidity ^0.8.0;
 
-import "../munged-simple/DoubleLinkedList.sol";
+import {DoubleLinkedList} from "../munged-simple/DoubleLinkedList.sol";
 
 contract MockDllSimple {
     using DoubleLinkedList for DoubleLinkedList.List;
